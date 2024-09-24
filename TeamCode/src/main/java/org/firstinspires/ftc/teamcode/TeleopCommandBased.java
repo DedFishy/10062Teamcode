bc1dfa62bed9690b2e07ab51ad89db8e8ff96143
DedFishy <boynegregg312@gmail.com>
2024-09-24 17:29:48 -0400

    Made a teleop op mode thats command based

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopCommandBased.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.commands.DriveCommand;
+import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
+
+@TeleOp(name = "Teleop Command Based")
+public class TeleopCommandBased extends CommandOpMode {
+
+    private DriveSubsystem driveSubsystem;
+    private DriveCommand driveCommand;
+
+    @Override
+    public void initialize() {
+        driveSubsystem = new DriveSubsystem(hardwareMap);
+        driveCommand = new DriveCommand(driveSubsystem, gamepad1);
+        driveSubsystem.setDefaultCommand(driveCommand);
+        register(driveSubsystem);
+    }
+}
